01c741103eac34dbc3c322d0cb8b4141d90a0cf4
edwin-27 <120422575+edwin-27@users.noreply.github.com>
2023-09-13 15:25:17 +0100

    Add files via upload

---
+++ src/com/example/mymath.java
@@ -0,0 +1,17 @@
+package com.example;
+
+public class mymath {
+
+    private int a;
+    private int b;
+
+    public mymath(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public void printAddOf2Numbers(){
+        System.out.println(a+b);
+
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,15 @@
+import com.example.mymath;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        mymath firstFunction = new mymath(2,3);
+        firstFunction.printAddOf2Numbers();
+
+    }
+
+
+
+
+}
